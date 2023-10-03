51e3aad0fcf0c32be2a2cb59954386f46eef7bdd
Saurav Saha <varuas@gmail.com>
2023-10-03 14:18:19 +0000

    Added teleop buttons

--- include/robot_gui/robot_gui.h
+++ include/robot_gui/robot_gui.h
@@ -5,6 +5,7 @@
 #include "std_msgs/Float64.h"
 #include <opencv2/opencv.hpp>
 #include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
 
 class RobotGUI {
 public:
@@ -13,10 +14,11 @@ public:
 
 private:
   ros::Subscriber robot_info_sub;
+  ros::Publisher twist_pub;
+  geometry_msgs::Twist twist_msg;
+  float linear_velocity_step = 0.1;
+  float angular_velocity_step = 0.1;
   robotinfo_msgs::RobotInfo10Fields robot_info_data;
-
-  void
-  robotInfoCallback(const robotinfo_msgs::RobotInfo10Fields::ConstPtr &msg);
-
   const std::string WINDOW_NAME = "Robot Teleop GUI";
+  void robotInfoCallback(const robotinfo_msgs::RobotInfo10Fields::ConstPtr &msg);
 };
--- src/robot_gui.cpp
+++ src/robot_gui.cpp
@@ -1,11 +1,13 @@
 #include "robot_gui/robot_gui.h"
 #include "robotinfo_msgs/RobotInfo10Fields.h"
+#include <geometry_msgs/Twist.h>
 #include <ros/ros.h>
 
 RobotGUI::RobotGUI() {
   ros::NodeHandle nh;
   robot_info_sub = nh.subscribe<robotinfo_msgs::RobotInfo10Fields>(
       "/robot_info", 1, &RobotGUI::robotInfoCallback, this);
+  twist_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 }
 
 void RobotGUI::robotInfoCallback(
@@ -15,7 +17,7 @@ void RobotGUI::robotInfoCallback(
 }
 
 void RobotGUI::run() {
-  cv::Mat frame = cv::Mat(200, 500, CV_8UC3);
+  cv::Mat frame = cv::Mat(800, 300, CV_8UC3);
 
   // Init a OpenCV window and tell cvui to use it.
   cv::namedWindow(WINDOW_NAME);
@@ -25,23 +27,49 @@ void RobotGUI::run() {
     // Fill the frame with a nice color
     frame = cv::Scalar(49, 52, 49);
 
-    // Create window at (40, 20) with size 250x80 (width x height) and title
-    cvui::window(frame, 40, 20, 250, 145, "Info");
+    // General Info Panel
+    cvui::window(frame, 30, 20, 250, 145, "Info");
+    cvui::text(frame, 30, 40, robot_info_data.data_field_01);
+    cvui::text(frame, 30, 55, robot_info_data.data_field_02);
+    cvui::text(frame, 30, 70, robot_info_data.data_field_03);
+    cvui::text(frame, 30, 85, robot_info_data.data_field_04);
+    cvui::text(frame, 30, 100, robot_info_data.data_field_05);
+    cvui::text(frame, 30, 115, robot_info_data.data_field_06);
+    cvui::text(frame, 30, 130, robot_info_data.data_field_07);
+    cvui::text(frame, 30, 145, robot_info_data.data_field_08);
 
-    cvui::text(frame, 40, 40, robot_info_data.data_field_01);
-    cvui::text(frame, 40, 55, robot_info_data.data_field_02);
-    cvui::text(frame, 40, 70, robot_info_data.data_field_03);
-    cvui::text(frame, 40, 85, robot_info_data.data_field_04);
-    cvui::text(frame, 40, 100, robot_info_data.data_field_05);
-    cvui::text(frame, 40, 115, robot_info_data.data_field_06);
-    cvui::text(frame, 40, 130, robot_info_data.data_field_07);
-    cvui::text(frame, 40, 145, robot_info_data.data_field_08);
-
-    // std::string robot_info_data = "Data Received" <<
-
-    // Show how many times the button has been clicked inside the window.
-    // cvui::printf(frame, 45, 45, 0.4, 0xff0000, "Data received: %s",
-    //  robot_info_data.data_field_01.c_str());
+    // Tele-op buttons
+    if (cvui::button(frame, 100, 180, " Forward ")) {
+      // The button was clicked, update the Twist message
+      twist_msg.linear.x = twist_msg.linear.x + linear_velocity_step;
+      // twist_pub.publish(twist_msg);
+    }
+    // Show a button at position x = 100, y = 50
+    if (cvui::button(frame, 100, 210, "   Stop  ")) {
+      // The button was clicked, update the Twist message
+      twist_msg.linear.x = 0.0;
+      twist_msg.angular.z = 0.0;
+      // twist_pub.publish(twist_msg);
+    }
+    // Show a button at position x = 30, y = 50
+    if (cvui::button(frame, 30, 210, " Left ")) {
+      // The button was clicked, update the Twist message
+      twist_msg.angular.z = twist_msg.angular.z + angular_velocity_step;
+      // twist_pub.publish(twist_msg);
+    }
+    // Show a button at position x = 195, y = 50
+    if (cvui::button(frame, 195, 210, " Right ")) {
+      // The button was clicked, update the Twist message
+      twist_msg.angular.z = twist_msg.angular.z - angular_velocity_step;
+      // twist_pub.publish(twist_msg);
+    }
+    // Show a button at position x = 100, y = 80
+    if (cvui::button(frame, 100, 240, "Backward")) {
+      // The button was clicked,update the Twist message
+      twist_msg.linear.x = twist_msg.linear.x - linear_velocity_step;
+      // twist_pub.publish(twist_msg);
+    }
+    twist_pub.publish(twist_msg);
 
     // Update cvui internal stuff
     cvui::update();
